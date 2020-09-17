254a4efb3355de9ac454c611bffd30b4d111dc9e
metal6 <metal@i0.tf>
2020-09-17 19:21:32 +0100

    test

--- src/main.ts
+++ src/main.ts
@@ -31,6 +31,7 @@ const run = async () => {
       const passedCheck = jwtChecks.find(jwtPayload => jwtPayload && jwtPayload.iss === `https://${process.env.LOGIN_DOMAIN!}`)
 
       if (passedCheck) {
+        console.log(`Target: ${process.env.TARGET_URL!}`)
         proxyServer.web(req, res, {
           target: process.env.TARGET_URL!
         })
@@ -45,6 +46,7 @@ const run = async () => {
 
   // Proxy WS
   customServer.on('upgrade', (req, socket, head) => {
+    console.log(`Doing ws`);
     proxyServer.ws(req, socket, head)
   })
 
