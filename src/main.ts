14acf5a14713ad58bd15d464315cd2f96ee01c50
metal <admin@metalruller.com>
2020-09-17 13:40:16 +0100

    add listen

--- src/main.ts
+++ src/main.ts
@@ -49,7 +49,7 @@ const run = async () => {
   })
 
   const httpPort = process.env.PORT || 80
-  customServer.listen(httpPort, () => {
+  customServer.listen(httpPort, process.env.LISTEN, () => {
     console.log(`Listening on port ${httpPort}`)
   })
 
